23ff9560bf6020eb0ba77444f0923d9126a2d1ef
Khalil <65265303+KhalilHsu@users.noreply.github.com>
2021-06-08 20:14:07 +0800

    Add files via upload

---
+++ Scripts/BalloonScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonScript : MonoBehaviour
+{
+
+
+
+
+    void Update()
+    {
+
+        transform.Translate(Vector3.back * Time.deltaTime * 0.2f);
+
+    }
+
+
+
+
+}
---
+++ Scripts/SpawnScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+    public GameObject[] balloons;
+
+
+    void Start()
+    {
+        StartCoroutine(StartSpawning());
+    }
+
+    IEnumerator StartSpawning()
+    {
+        yield return new WaitForSeconds(3);
+
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(balloons[i], spawnPoints[i].position, Quaternion.identity);
+        }
+
+        StartCoroutine(StartSpawning());
+    }
+
+
+}
